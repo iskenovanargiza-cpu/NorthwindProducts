ef4ac0507f4b269e4691b3a1d15eb416b24f2c74
novyjpolzovatel <iskenova.nargiza@gmail.com>
2025-12-02 15:35:38 -0500

    Add a home screen - Display all categories by Category ID user selected in products

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,13 +1,11 @@
 package com.pluralsight;
-
 import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-
-        Scanner scanner = new Scanner(System.in);
 
         Connection connection = null;
         PreparedStatement statement = null;
@@ -20,18 +18,20 @@ public class Main {
 
         try {
             connection = DriverManager.getConnection(url,user,password);
-            System.out.println("What do you want to do?\n1) Display all products\n2) Display all customers\n0) Exit\nSelect an option:");
+            System.out.println("What do you want to do?\n1) Display all products\n2) Display all customers\n3) Display all categories\n0) Exit\nSelect an option:");
             int userChoice = scanner.nextInt();
             scanner.nextLine();
 
             switch(userChoice) {
                 case 1 -> displayProducts(connection);
                 case 2 -> displayCustomers(connection);
-                case 0 ->
-                        System.exit(0);
+                case 3 -> displayCategories(connection);
+                case 0 -> System.exit(0);
+                default -> System.out.println("Entered wrong number. Please try again!");
             }
         } catch (SQLException e) {
             e.printStackTrace();
+
         } finally {
 
             if (results != null) {
@@ -91,4 +91,40 @@ public class Main {
             System.out.printf("Contact Name: %s\nCompany Name: %s\nCity: %s\nCountry:%s\nPhone: %s\n--------------------\n",contactName,companyName,city,country,phoneNumber);
         }
     }
+
+    public static void displayCategories(Connection connection) throws SQLException {
+        PreparedStatement statement;
+        ResultSet results;
+
+        String categoriesQuery = "SELECT * FROM Categories ORDER BY CategoryID";
+        statement = connection.prepareStatement(categoriesQuery);
+        System.out.println();
+        results = statement.executeQuery();
+
+        while (results.next()) {
+            int categoryID = results.getInt("CategoryID");
+            String categoryName = results.getString("CategoryName");
+            System.out.printf("Category ID: %s\nCategory Name: %s\n--------------------\n", categoryID, categoryName);
+
+        }
+
+        System.out.println("Please enter category ID number:");
+        int userChoice = scanner.nextInt();
+        scanner.nextLine();
+        String productDisplay = "SELECT * FROM Products WHERE CategoryID = ?";
+        statement = connection.prepareStatement(productDisplay);
+        System.out.println();
+        statement.setInt(1, userChoice);
+
+        results = statement.executeQuery();
+
+        while (results.next()) {
+            int productID = results.getInt("ProductID");
+            String productName = results.getString("ProductName");
+            double unitPrice = results.getDouble("UnitPrice");
+            int unitsInStock = results.getInt("UnitsInStock");
+            System.out.printf("Product ID: %s\nProduct Name: %s\nPrice: %s\nStock:%s\n--------------------\n",productID,productName,unitPrice,unitsInStock);
+        }
+    }
 }
+
